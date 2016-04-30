11a56d8c4582a1c5460eb14917adb84df7133f36
Justin Hempsey <flinestone@rocketmail.com>
2016-04-30 16:21:10 -0700

    Finished confirmation page

--- public_html/php/confirm.php
+++ public_html/php/confirm.php
@@ -1,3 +1,15 @@
+<html>
+    <head>
+        <title>Confirmation</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="../stylesheets/home.css">
+        <link rel="shortcut icon" href="../images/favicon.ico" type="../image/x-icon">
+        <link rel="icon" href="../images/favicon.ico" type="../image/x-icon">
+    </head>
+    <body>
+        <h1>Peter sells peppers</h1>
+        <h2>Order Confirmation</h2>
 <?php
     $confirm_id = htmlspecialchars($_GET["id"]);
     $servername = "sylvester-mccoy-v3.ics.uci.edu";
@@ -30,27 +42,64 @@
                 //addr
                 echo "<td>Address: </td>";
                 echo "<td>".$row['street_addr']."<td>";
+                echo "</tr><tr><td>Zip Code:</td>";
                 echo "<td>".$row['zip']."</td>";
+                echo "</tr><tr><td>State:</td>";
                 echo "<td>".$row['state']."</td>";
                 echo "</tr>";
 
                 echo "<tr>";
-                //pepper
-                //TODO
+                //ship speed
+                echo "<td> Shipping Speed:</td>";
+                echo "<td>";
+                switch ($row['ship_speed']) {
+                    case 1:
+                        echo "One day";
+                        break;
+                    case 2:
+                        echo "Two days";
+                        break;
+                    case 3:
+                        echo "1-2 weeks";
+                        break;
+                    default:
+                    //do nothing
+                }
+                echo "</td>";
                 echo "</tr>";
 
                 echo "<tr>";
-                //ship speed
-                //TODO
+                echo "<td> Quantity:</td>";
+                echo "<td>";
+                echo $row['quantity'];
+                $sql = "SELECT * FROM peppers WHERE ID=".$row['pepper_id'];
+                $stmt = $conn->query($sql);
+                $stmt->execute();
+                echo "</tr><tr><td>Pepper:</td><td>";
+                while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+                    echo $row['pepper_name'];
+                }
+                echo "</td>";
                 echo "</tr>";
+
             } echo "</table>\n";
 
         } else {
-            echo "goodbye";
+            ?>
+            <div class='center-div'>
+                ERROR: Invalid order ID number.
+            </div>
+            <?php
         }
     } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
+
+        //echo "Connection failed: " . $e->getMessage();
     }
 
 ?>
-<br>
+        <br>
+        <div class='center-div'>
+            <b>Thanks for ordering! Hope to burn you again soon!</b>
+        </div>
+    </body>
+</html>
--- public_html/php/form.php
+++ public_html/php/form.php
@@ -1,41 +1,12 @@
 <?php
-    require 'Models/Order.php';
-    require 'Repositories/OrdersRepository.php';
-    
-    if (isset($_POST['action'])) {
+if (isset($_POST['action'])) {
     switch($_POST['action']) {
     case 'buy':
         //Add to sql table
-        $pepper_id = $_POST['pepper_id'];
-        $first_name = $_POST['first_name'];
-        $last_name = $_POST['last_name'];
-        $credit_card = $_POST['credit_card'];
-        $address = $_POST['address'];
-        $phone = $_POST['phone'];
-        $zip_code = $_POST['zip_code'];
-        $state = $_POST['state'];
-        $quantity = $_POST['quantity'];
-        $ship = $_POST['ship'];
-        
-        $order = new Order;
-        $order->pepper_id = $pepper_id;
-        $order->first_name = $first_name;
-        $order->last_name = $last_name;
-        $order->credit_card = $credit_card;
-        $order->address = $address;
-        $order->phone = $phone;
-        $order->zip_code = $zip_code;
-        $order->state = $state;
-        $order->quantity = $quantity;
-        $order->ship = $ship;
-        
-        $ordersRepository = new OrdersRepository;
-        $ordersRepository->createOrder($order);
-        break;
-    case 'order':
         //redirect to confirmation page
         break;
     default:
+        //Do nothing
     }
-  }
+}
 ?>
