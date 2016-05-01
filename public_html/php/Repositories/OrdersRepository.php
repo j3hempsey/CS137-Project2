d4425285b47740a1e49eb7110d37da6783f54d22
peterhn <peterhn2@uci.edu>
2016-05-01 15:07:42 -0700

    fixed string concat in order repo

--- public_html/php/Repositories/OrdersRepository.php
+++ public_html/php/Repositories/OrdersRepository.php
@@ -4,8 +4,8 @@
      
         function createOrder($order) {
             $conn =  DBContext::getSqlConnection();
-            $sql = "INSERT INTO Orders (first_name, last_name, cc_number, street_addr, phone_num, zip, state, pepper_id, quantity, ship_speed)" +
-                   "VALUES ('" + $order->first_name +"', '"+ $order->last_name +"', " + $order->credit_card +", '" + $order->address + "', " + $order->phone + ", " + $order->zip + ", '" + $order->state +"', " + $order->pepper_id +", " + $order->ship+")";
+            $sql = "INSERT INTO Orders (first_name, last_name, cc_number, street_addr, phone_num, zip, state, pepper_id, quantity, ship_speed)" .
+                   "VALUES ('" . $order->first_name ."', '". $order->last_name ."', " . $order->credit_card .", '" . $order->address . "', " . $order->phone . ", " . $order->zip . ", '" . $order->state ."', " . $order->pepper_id .", " . $order->ship.")";
            echo $sql;
            //$stmt = $conn->query($sql);
            //if($stmt) { return true; }
