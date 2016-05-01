f7e66746abe9a2d805787ee74292149a5610fed1
peterhn <peterhn2@uci.edu>
2016-05-01 15:28:48 -0700

    begin transaction

--- public_html/php/Repositories/OrdersRepository.php
+++ public_html/php/Repositories/OrdersRepository.php
@@ -6,7 +6,7 @@
             $conn =  DBContext::getSqlConnection();
             $stmt = $conn->prepare("INSERT INTO orders (first_name, last_name, cc_number, street_addr, phone_num, zip, state, pepper_id, quantity, ship_speed) VALUES(?,?,?,?,?,?,?,?,?,?)"); 
             try{
-                $conn->prepareTransaction();
+                $conn->beginTransaction();
                 $stmt->execute(array($order->first_name, $order->last_name, $order->credit_card, $order->address, $order->phone, $order->zip_code, $order->state, $order->pepper_id, $order->quantity, $order->ship));
                 $conn->commit();
                 $orderId = $conn->lastInsertId();
