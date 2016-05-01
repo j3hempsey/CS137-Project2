137bc79cf901741de0ddd0048cc61a1829e64909
peterhn <peterhn2@uci.edu>
2016-05-01 15:02:47 -0700

    updated connection string in order repo

--- public_html/php/Repositories/OrdersRepository.php
+++ public_html/php/Repositories/OrdersRepository.php
@@ -8,7 +8,7 @@
                 $username = "inf124grp13";
                 $password = "4a=eDuVu";
                 //Make SQL connection and get my answer.
-                $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+                $conn = new PDO("mysql:host=sylvester-mccoy-v3.ics.uci.edu;dbname=inf124grp13", $username, $password);
                 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $sql = "INSERT INTO Orders (first_name, last_name, cc_number, street_addr, phone_num, zip, state, pepper_id, quantity, ship_speed)" +
                    "VALUES ('" + $order->first_name +"', '"+ $order->last_name +"', '" + $order->credit_card +"', '" + $order->address + "', '" + $order->phone + "', '" + $order->zip + "', '" + $order->state +"', '" + $order->pepper_id +"', '" + $order->ship+"')";
