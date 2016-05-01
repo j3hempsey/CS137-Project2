847452bfb6d4a09d110b5c9652ba0b7e94a5732d
peterhn <peterhn2@uci.edu>
2016-05-01 15:06:53 -0700

    removed order db commit

--- public_html/php/Repositories/OrdersRepository.php
+++ public_html/php/Repositories/OrdersRepository.php
@@ -7,8 +7,8 @@
             $sql = "INSERT INTO Orders (first_name, last_name, cc_number, street_addr, phone_num, zip, state, pepper_id, quantity, ship_speed)" +
                    "VALUES ('" + $order->first_name +"', '"+ $order->last_name +"', " + $order->credit_card +", '" + $order->address + "', " + $order->phone + ", " + $order->zip + ", '" + $order->state +"', " + $order->pepper_id +", " + $order->ship+")";
            echo $sql;
-           $stmt = $conn->query($sql);
-           if($stmt) { return true; }
+           //$stmt = $conn->query($sql);
+           //if($stmt) { return true; }
            
            return false; 
         }
