2ac5fd939f6ab4e591cad57575fefe41c82ede5b
peterhn <peterhn2@uci.edu>
2016-05-01 15:05:21 -0700

    removed sql quotes from order repo

--- public_html/php/Repositories/OrdersRepository.php
+++ public_html/php/Repositories/OrdersRepository.php
@@ -3,15 +3,9 @@
     class OrdersRepository {
      
         function createOrder($order) {
-            $servername = "sylvester-mccoy-v3.ics.uci.edu";
-                $db="inf124grp13";
-                $username = "inf124grp13";
-                $password = "4a=eDuVu";
-                //Make SQL connection and get my answer.
-                $conn = new PDO("mysql:host=sylvester-mccoy-v3.ics.uci.edu;dbname=inf124grp13", $username, $password);
-                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn =  DBContext::getSqlConnection();
             $sql = "INSERT INTO Orders (first_name, last_name, cc_number, street_addr, phone_num, zip, state, pepper_id, quantity, ship_speed)" +
-                   "VALUES ('" + $order->first_name +"', '"+ $order->last_name +"', '" + $order->credit_card +"', '" + $order->address + "', '" + $order->phone + "', '" + $order->zip + "', '" + $order->state +"', '" + $order->pepper_id +"', '" + $order->ship+"')";
+                   "VALUES ('" + $order->first_name +"', '"+ $order->last_name +"', " + $order->credit_card +", '" + $order->address + "', " + $order->phone + ", " + $order->zip + ", '" + $order->state +"', " + $order->pepper_id +", " + $order->ship+")";
            echo $sql;
            $stmt = $conn->query($sql);
            if($stmt) { return true; }
