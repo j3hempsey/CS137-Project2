35975a474fdbc3f89c701ab9c91385c615196e12
peterhn <peterhn2@uci.edu>
2016-05-01 13:58:28 -0700

    added order creation

--- public_html/php/Repositories/DBContext.php
+++ public_html/php/Repositories/DBContext.php
@@ -1,16 +1,17 @@
 <?php
     class DBContext {
-        private $servername = "sylvester-mccoy-v3.ics.uci.edu";
-        private $db="inf124grp13";
-        private $username = "inf124grp13";
-        private $password = "4a=eDuVu";
-
         static function getSqlConnection() {
             try {
-                $conn = new PDO("mysql:host=sylvester-mccoy-v3.ics.uci.edu;dbname=inf124grp13;", "inf124grp13", "4a=eDuVu");
-                // set the PDO error mode to exception
+                $servername = "sylvester-mccoy-v3.ics.uci.edu";
+                $db="inf124grp13";
+                $username = "inf124grp13";
+                $password = "4a=eDuVu";
+                //Make SQL connection and get my answer.
+                $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
                 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                
+                if($conn->connect_error) {
+                    echo "DATABASE CONNECTION FAILED.";
+                }
                 return $conn;
             } catch(PDOException $e) {
                     print "Connection failed: " . $e->getMessage();
