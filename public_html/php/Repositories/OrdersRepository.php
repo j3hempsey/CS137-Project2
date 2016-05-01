0425a32d533ed8915dee6797878459e5f0943d82
peterhn <peterhn2@uci.edu>
2016-05-01 15:12:32 -0700

    fixed query in order repo

--- public_html/php/Repositories/OrdersRepository.php
+++ public_html/php/Repositories/OrdersRepository.php
@@ -5,7 +5,7 @@
         function createOrder($order) {
             $conn =  DBContext::getSqlConnection();
             $sql = "INSERT INTO orders (first_name, last_name, cc_number, street_addr, phone_num, zip, state, pepper_id, quantity, ship_speed)" .
-                   "VALUES ('" . $order->first_name ."', '". $order->last_name ."', " . $order->credit_card .", '" . $order->address . "', " . $order->phone . ", " . $order->zip_code . ", '" . $order->state ."', " . $order->pepper_id .", " . $order->ship.")";
+                   "VALUES ('" . $order->first_name ."', '". $order->last_name ."', " . $order->credit_card .", '" . $order->address . "', " . $order->phone . ", " . $order->zip_code . ", '" . $order->state ."', " . $order->pepper_id .", " . $order->pepper_id  .", ". $order->ship.")";
            echo $sql;
            $stmt = $conn->query($sql);
            if($stmt) { return true; }
