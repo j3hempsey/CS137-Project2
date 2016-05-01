999bcdce6a405e4b46ad19b493f077583636f3c1
peterhn <peterhn2@uci.edu>
2016-05-01 15:00:48 -0700

    test removed dbcontext from orders repot

--- public_html/php/Repositories/OrdersRepository.php
+++ public_html/php/Repositories/OrdersRepository.php
@@ -3,7 +3,13 @@
     class OrdersRepository {
      
         function createOrder($order) {
-            $conn =  DBContext::getSqlConnection();
+            $servername = "sylvester-mccoy-v3.ics.uci.edu";
+                $db="inf124grp13";
+                $username = "inf124grp13";
+                $password = "4a=eDuVu";
+                //Make SQL connection and get my answer.
+                $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $sql = "INSERT INTO Orders (first_name, last_name, cc_number, street_addr, phone_num, zip, state, pepper_id, quantity, ship_speed)" +
                    "VALUES ('" + $order->first_name +"', '"+ $order->last_name +"', '" + $order->credit_card +"', '" + $order->address + "', '" + $order->phone + "', '" + $order->zip + "', '" + $order->state +"', '" + $order->pepper_id +"', '" + $order->ship+"')";
            echo $sql;
