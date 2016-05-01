e1ab2fa47a6d8a8a890a45bd7374d62df4b91fa1
peterhn <peterhn2@uci.edu>
2016-05-01 15:25:46 -0700

    added order id return to order repot

--- public_html/php/Repositories/OrdersRepository.php
+++ public_html/php/Repositories/OrdersRepository.php
@@ -4,13 +4,26 @@
      
         function createOrder($order) {
             $conn =  DBContext::getSqlConnection();
-            $sql = "INSERT INTO orders (first_name, last_name, cc_number, street_addr, phone_num, zip, state, pepper_id, quantity, ship_speed)" .
-                   "VALUES ('" . $order->first_name ."', '". $order->last_name ."', " . $order->credit_card .", '" . $order->address . "', " . $order->phone . ", " . $order->zip_code . ", '" . $order->state ."', " . $order->pepper_id .", " . $order->pepper_id  .", ". $order->ship.")";
-           echo $sql;
-           $stmt = $conn->query($sql);
-           if($stmt) { return true; }
+            $stmt = $conn->prepare("INSERT INTO orders (first_name, last_name, cc_number, street_addr, phone_num, zip, state, pepper_id, quantity, ship_speed) VALUES(?,?,?,?,?,?,?,?,?,?)"); 
+            try{
+                $conn->prepareTransaction();
+                $stmt->execute(array($order->first_name, $order->last_name, $order->credit_card, $order->address, $order->phone, $order->zip_code, $order->state, $order->pepper_id, $order->quantity, $order->ship));
+                $conn->commit();
+                $orderId = $conn->lastInsertId();
+                
+                return $orderId;
+            } catch(PDOException $e) {
+                $conn->rollback(); 
+                print "Error!: " . $e->getMessage() . "</br>"; 
+            }
+            
+            //$sql = "INSERT INTO orders (first_name, last_name, cc_number, street_addr, phone_num, zip, state, pepper_id, quantity, ship_speed)" .
+            //       "VALUES ('" . $order->first_name ."', '". $order->last_name ."', " . $order->credit_card .", '" . $order->address . "', " . $order->phone . ", " . $order->zip_code . ", '" . $order->state ."', " . $order->pepper_id .", " . $order->pepper_id  .", ". $order->ship.")";
+   
+           //$stmt = $conn->query($sql);
+           //if($stmt) { return true; }
            
-           return false; 
+           return -1; 
         }
     }
 ?>
